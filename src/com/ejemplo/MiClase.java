e4e2ad49dc44a933c2795df92c4ba89473367cea
José Gaspar Sánchez Garcia <josegasparsanch@gmail.com>
2021-07-15 18:08:30 +0200

    ProbandoAntv1.0.0.jar

--- src/com/ejemplo/MiClase.java
+++ src/com/ejemplo/MiClase.java
@@ -4,7 +4,7 @@ public class MiClase {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("!!!Esto es una prueba de Ant con Eclipse!!!!!");
+		System.out.println("!!! Esto es una prueba de Ant con Eclipse !!!!!");
 	}
 
 }
