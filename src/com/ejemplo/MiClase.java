2e6f9686d5a1dac5b4bb83998581fc091c2bf6fc
José Gaspar Sánchez Garcia <josegasparsanch@gmail.com>
2021-07-15 17:41:03 +0200

    Modificación a subir

---
+++ src/com/ejemplo/MiClase.java
@@ -0,0 +1,10 @@
+package com.ejemplo;
+
+public class MiClase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Esto es una prueba de Ant con Eclipse!!!");
+	}
+
+}
